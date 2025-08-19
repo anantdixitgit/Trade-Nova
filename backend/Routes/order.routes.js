20bbe126d515ca67066bdaefc308073d8886853d
Anant Dixit <147609316+anantdixitgit@users.noreply.github.com>
2025-08-19 19:38:53 +0530

    Update order.routes.js

--- backend/Routes/order.routes.js
+++ backend/Routes/order.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { allorder, newOrder } = require("../controllers/order.controller.js");
 
-router.route("/allorders").get(allorder).post(newOrder);
+router.route("/allorders").get(allorder);
+router.route("/newOrder").post(newOrder);
 
 module.exports = router;
